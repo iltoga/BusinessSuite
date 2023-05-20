604a0fc35862fca9554de4cf4b0d85eaa3a65403
Stefano Galassi <stefano.galassi@gotoko.co.id>
2023-05-20 20:52:57 +0800

    Turn debug true for prod

--- RevisBaliCRM/settings/prod.py
+++ RevisBaliCRM/settings/prod.py
@@ -13,6 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['crm.revisbali.com', 'www.revisbali.com']
