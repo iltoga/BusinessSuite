7d4a0e7d6b5a76703e56c5b6313b44e2829dec4f
Stefano Galassi <stefano.galassi@gotoko.co.id>
2023-05-29 08:45:19 +0800

    Update migrations

--- core/migrations/0002_create_groups.py
+++ core/migrations/0002_create_groups.py
@@ -9,11 +9,12 @@ def generate_groups(apps, schema_editor):
 class Migration(migrations.Migration):
     dependencies = [
         # Also add any other migrations that introduce new models here
-        ('customers', '0001_initial'),
-        ('products', '0001_initial'),
-        ('invoices', '0001_initial'),
-        ('transactions', '0001_initial'),
-        ('customer_applications', '0001_initial'),
+        ('core', '0001_initial'),
+        # ('customers', '0001_initial'),
+        # ('products', '0001_initial'),
+        # ('invoices', '0001_initial'),
+        # ('transactions', '0001_initial'),
+        # ('customer_applications', '0001_initial'),
     ]
     operations = [
         migrations.RunPython(generate_groups),
