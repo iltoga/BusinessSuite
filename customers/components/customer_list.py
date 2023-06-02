0a3d26f3f3db30c471677b01ddc5de0e7d4dc21e
Stefano Galassi <stefano.galassi@gotoko.co.id>
2023-06-02 14:49:51 +0800

    Update components for templates

--- customers/components/customer_list.py
+++ customers/components/customer_list.py
@@ -1,15 +1,19 @@
 from django_unicorn.components import UnicornView
 from customers.models import Customer
-
+from math import ceil
 
 class CustomerListView(UnicornView):
     customers = []
     search_input = ''
     page = 1
-    items_per_page = 7
+    items_per_page = 3
     start_search_at = 3 # start searching after 3 characters
+    total_customers = 0
+    total_pages = 0
 
     def mount(self):
+        self.total_customers = Customer.objects.count()
+        self.total_pages = ceil(self.total_customers / self.items_per_page)
         self.load_customers()
 
     def load_customers(self):
@@ -18,7 +22,11 @@ class CustomerListView(UnicornView):
     def search(self):
         if self.search_input and len(self.search_input) >= self.start_search_at:
             self.customers = list(Customer.objects.search_customers(self.search_input).values())
+            self.total_customers = len(self.customers)
+            self.total_pages = ceil(self.total_customers / self.items_per_page)
         else:
+            self.total_customers = Customer.objects.count()
+            self.total_pages = ceil(self.total_customers / self.items_per_page)
             self.load_customers()
 
     def next_page(self):
@@ -29,3 +37,8 @@ class CustomerListView(UnicornView):
         self.page -= 1 if self.page > 1 else 1
         self.load_customers()
 
+    def has_previous(self):
+        return self.page > 1
+
+    def has_next(self):
+        return self.page * self.items_per_page < self.total_customers
