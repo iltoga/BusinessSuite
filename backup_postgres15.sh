e702283f8be57f70865004732b301c15ba7d0a02
iltoga <galaxy73.it@gmail.com>
2025-11-06 10:48:45 +0800

    fix backup

--- backup_postgres15.sh
+++ backup_postgres15.sh
@@ -11,9 +11,12 @@ TMP_PATH="/tmp/${BACKUP_NAME}"
 HOST_PATH="./${BACKUP_NAME}"
 
 echo "🔹 Dumping all databases from container: postgres-srv"
-docker exec -t postgres-srv pg_dumpall -U "$DB_USER" > "$TMP_PATH"
+docker exec postgres-srv sh -c "pg_dumpall -U \"$DB_USER\" -f \"$TMP_PATH\""
 
 echo "🔹 Copying dump to host..."
 docker cp postgres-srv:"$TMP_PATH" "$HOST_PATH"
+
+echo "🔹 Cleaning up dump inside container..."
+docker exec postgres-srv rm -f "$TMP_PATH"
 
 echo "✅ Backup complete: $HOST_PATH"
