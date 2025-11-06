cdfda4c01874f6486fe6a0d0f53fdc48e9101a34
iltoga <galaxy73.it@gmail.com>
2025-11-06 21:42:16 +0800

    Add application counts to product list
    * Annotated Product queryset with application_count: added get_queryset to ProductListView and annotate(..., application_count=models.Count('doc_applications')).
    * Display application_count in template: added "Applications" column and render product.application_count in product_list.html.
    * Adjust imports: import django.db.models alongside other imports for Count usage.

--- products/components/product_list.py
+++ products/components/product_list.py
@@ -1,3 +1,5 @@
+from django.db import models
+
 from core.components.unicorn_search_list_view import UnicornSearchListView
 from products.models import Product
 
@@ -6,3 +8,11 @@ class ProductListView(UnicornSearchListView):
     model = Product
     model_search_method = "search_products"
     items_per_page = 15
+
+    def get_queryset(self):
+        # Get the base queryset from the parent
+        queryset = super().get_queryset()
+        # Annotate with application count
+        from customer_applications.models.doc_application import DocApplication
+
+        return queryset.annotate(application_count=models.Count("doc_applications"))
